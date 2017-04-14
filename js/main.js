f6262f5207e008614da416fb17a68d47d43c7e85
apres95 <alexisepresbrey@gmail.com>
2017-04-14 16:46:49 -0700

    first commmit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	var append = true;
+
+	$('.keys').on('click', function(){
+		var value = $(this).val();
+			$('.calculation').append(value);
+	});
+
+	// $('#orange').on('click' function(){
+
+	// })
+
+	$('button[name="equals"]').on('click', function(){
+		var calc = $('.calculation').text();
+		var answer = eval(calc);
+			$('.calculation').text(answer);
+	});
+
+	$('button[name="squareRoot"]').on('click', function(){
+		var calc = $('.calculation').text();
+		var answer = Math.sqrt(calc);
+			$('.calculation').text('')
+			$('.calculation').append(answer);
+
+	});
+
+	$('button[name="clear"]').on('click', function(){
+		$('.calculation').text('0');
+	});
+
+
+	$('button[name="back"]').on('click', function(){
+
+		var calc = $('.calculation').text();
+		var shortString = calc.substr(0, calc.length- 1);
+		$('.calculation').text(shortString);
+	});
+	
+
+});
+
+
+
+
